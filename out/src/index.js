e0f52e5bc68982b917f70f8dee4ef5292f646191
Pierre Marchand <pierre-m@atelier-cartographique.be>
2020-12-06 15:55:33 +0100

    udpated deps

--- out/src/index.js
+++ out/src/index.js
@@ -1,154 +1,165 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const io = require("io-ts");
-exports.i = io.interface;
-exports.u = io.union;
-exports.l = io.literal;
-exports.a = io.array;
-exports.t = io.tuple;
-exports.p = io.partial;
-exports.DirectGeometryTypeIO = exports.u([
-    exports.l('Point'),
-    exports.l('Polygon'),
-    exports.l('LineString'),
-    exports.l('MultiPoint'),
-    exports.l('MultiPolygon'),
-    exports.l('MultiLineString'),
-], 'DirectGeometryTypeIO');
-exports.GeometryCollectionTypeIO = exports.l('GeometryCollection', 'GeometryCollectionTypeIO');
-exports.GeometryTypeIO = exports.u([
-    exports.DirectGeometryTypeIO,
-    exports.GeometryCollectionTypeIO,
-], 'GeometryTypeIO');
-exports.GeoJsonTypeIO = exports.u([
-    exports.GeometryTypeIO,
-    exports.u([
-        exports.l('Feature'),
-        exports.l('FeatureCollection'),
-    ]),
-], 'GeoJsonTypeIO');
-exports.BoundingBoxIO = exports.u([exports.a(exports.a(io.number)), exports.a(exports.a(exports.a(io.number)))], 'BoundingBoxIO');
-exports.GeoJsonObjectIO = io.intersection([
-    exports.i({
-        type: exports.GeoJsonTypeIO,
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'GeoJsonObjectIO');
-exports.PositionIO = exports.a(io.number, 'PositionIO');
-exports.CoordinatesIO = exports.u([
-    exports.PositionIO,
-    exports.a(exports.PositionIO),
-    exports.a(exports.a(exports.PositionIO)),
-    exports.a(exports.a(exports.a(exports.PositionIO))),
-], 'CoordinatesIO');
-exports.DirectGeometryObjectIO = io.intersection([
-    exports.i({
-        type: exports.DirectGeometryTypeIO,
-        coordinates: exports.CoordinatesIO,
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'DirectGeometryObjectIO');
-exports.PointIO = io.intersection([
-    exports.i({
-        type: exports.l('Point'),
-        coordinates: exports.PositionIO,
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'PointIO');
-exports.MultiPointIO = io.intersection([
-    exports.i({
-        type: exports.l('MultiPoint'),
-        coordinates: exports.a(exports.PositionIO),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'MultiPointIO');
-exports.LineStringIO = io.intersection([
-    exports.i({
-        type: exports.l('LineString'),
-        coordinates: exports.a(exports.PositionIO),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'LineStringIO');
-exports.MultiLineStringIO = io.intersection([
-    exports.i({
-        type: exports.l('MultiLineString'),
-        coordinates: exports.a(exports.a(exports.PositionIO)),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'MultiLineStringIO');
-exports.PolygonIO = io.intersection([
-    exports.i({
-        type: exports.l('Polygon'),
-        coordinates: exports.a(exports.a(exports.PositionIO)),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'PolygonIO');
-exports.MultiPolygonIO = io.intersection([
-    exports.i({
-        type: exports.l('MultiPolygon'),
-        coordinates: exports.a(exports.a(exports.a(exports.PositionIO))),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'MultiPolygonIO');
-exports.GeometryCollectionIO = io.intersection([
-    exports.i({
-        type: exports.l('GeometryCollection'),
-        geometries: exports.a(exports.DirectGeometryObjectIO),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-]);
-exports.GeometryObjectIO = exports.u([
-    exports.DirectGeometryObjectIO,
-    exports.GeometryCollectionIO,
-], 'GeometryObjectIO');
-exports.PropertiesIO = exports.u([io.dictionary(io.string, io.any), io.null], 'PropertiesIO');
-exports.FeatureIO = io.intersection([
-    exports.i({
-        type: exports.l('Feature'),
-        geometry: exports.u([exports.GeometryObjectIO, io.null]),
-        properties: exports.PropertiesIO,
-    }),
-    exports.p({
-        id: exports.u([io.string, io.number]),
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'FeatureIO');
-exports.PartialFeatureIO = (propType, name) => io.intersection([
-    exports.i({
-        type: exports.l('Feature'),
-        geometry: exports.u([exports.GeometryObjectIO, io.null]),
-        properties: propType,
-    }),
-    exports.p({
-        id: exports.u([io.string, io.number]),
-        bbox: exports.BoundingBoxIO,
-    }),
-], name);
-exports.FeatureCollectionIO = io.intersection([
-    exports.i({
-        type: exports.l('FeatureCollection'),
-        features: exports.a(exports.FeatureIO),
-    }),
-    exports.p({
-        bbox: exports.BoundingBoxIO,
-    }),
-], 'FeatureCollectionIO');
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "io-ts"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.FeatureCollectionIO = exports.PartialFeatureIO = exports.FeatureIO = exports.PropertiesIO = exports.GeometryObjectIO = exports.GeometryCollectionIO = exports.MultiPolygonIO = exports.PolygonIO = exports.MultiLineStringIO = exports.LineStringIO = exports.MultiPointIO = exports.PointIO = exports.DirectGeometryObjectIO = exports.CoordinatesIO = exports.PositionIO = exports.GeoJsonObjectIO = exports.BoundingBoxIO = exports.GeoJsonTypeIO = exports.GeometryTypeIO = exports.GeometryCollectionTypeIO = exports.DirectGeometryTypeIO = exports.p = exports.t = exports.a = exports.l = exports.u = exports.i = void 0;
+    const io = require("io-ts");
+    exports.i = io.interface;
+    exports.u = io.union;
+    exports.l = io.literal;
+    exports.a = io.array;
+    exports.t = io.tuple;
+    exports.p = io.partial;
+    exports.DirectGeometryTypeIO = exports.u([
+        exports.l('Point'),
+        exports.l('Polygon'),
+        exports.l('LineString'),
+        exports.l('MultiPoint'),
+        exports.l('MultiPolygon'),
+        exports.l('MultiLineString'),
+    ], 'DirectGeometryTypeIO');
+    exports.GeometryCollectionTypeIO = exports.l('GeometryCollection', 'GeometryCollectionTypeIO');
+    exports.GeometryTypeIO = exports.u([
+        exports.DirectGeometryTypeIO,
+        exports.GeometryCollectionTypeIO,
+    ], 'GeometryTypeIO');
+    exports.GeoJsonTypeIO = exports.u([
+        exports.GeometryTypeIO,
+        exports.u([
+            exports.l('Feature'),
+            exports.l('FeatureCollection'),
+        ]),
+    ], 'GeoJsonTypeIO');
+    exports.BoundingBoxIO = exports.u([exports.a(exports.a(io.number)), exports.a(exports.a(exports.a(io.number)))], 'BoundingBoxIO');
+    exports.GeoJsonObjectIO = io.intersection([
+        exports.i({
+            type: exports.GeoJsonTypeIO,
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'GeoJsonObjectIO');
+    exports.PositionIO = exports.a(io.number, 'PositionIO');
+    exports.CoordinatesIO = exports.u([
+        exports.PositionIO,
+        exports.a(exports.PositionIO),
+        exports.a(exports.a(exports.PositionIO)),
+        exports.a(exports.a(exports.a(exports.PositionIO))),
+    ], 'CoordinatesIO');
+    exports.DirectGeometryObjectIO = io.intersection([
+        exports.i({
+            type: exports.DirectGeometryTypeIO,
+            coordinates: exports.CoordinatesIO,
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'DirectGeometryObjectIO');
+    exports.PointIO = io.intersection([
+        exports.i({
+            type: exports.l('Point'),
+            coordinates: exports.PositionIO,
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'PointIO');
+    exports.MultiPointIO = io.intersection([
+        exports.i({
+            type: exports.l('MultiPoint'),
+            coordinates: exports.a(exports.PositionIO),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'MultiPointIO');
+    exports.LineStringIO = io.intersection([
+        exports.i({
+            type: exports.l('LineString'),
+            coordinates: exports.a(exports.PositionIO),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'LineStringIO');
+    exports.MultiLineStringIO = io.intersection([
+        exports.i({
+            type: exports.l('MultiLineString'),
+            coordinates: exports.a(exports.a(exports.PositionIO)),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'MultiLineStringIO');
+    exports.PolygonIO = io.intersection([
+        exports.i({
+            type: exports.l('Polygon'),
+            coordinates: exports.a(exports.a(exports.PositionIO)),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'PolygonIO');
+    exports.MultiPolygonIO = io.intersection([
+        exports.i({
+            type: exports.l('MultiPolygon'),
+            coordinates: exports.a(exports.a(exports.a(exports.PositionIO))),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'MultiPolygonIO');
+    exports.GeometryCollectionIO = io.intersection([
+        exports.i({
+            type: exports.l('GeometryCollection'),
+            geometries: exports.a(exports.DirectGeometryObjectIO),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ]);
+    exports.GeometryObjectIO = exports.u([
+        exports.DirectGeometryObjectIO,
+        exports.GeometryCollectionIO,
+    ], 'GeometryObjectIO');
+    exports.PropertiesIO = exports.u([io.dictionary(io.string, io.any), io.null], 'PropertiesIO');
+    exports.FeatureIO = io.intersection([
+        exports.i({
+            type: exports.l('Feature'),
+            geometry: exports.u([exports.GeometryObjectIO, io.null]),
+            properties: exports.PropertiesIO,
+        }),
+        exports.p({
+            id: exports.u([io.string, io.number]),
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'FeatureIO');
+    exports.PartialFeatureIO = (propType, name) => io.intersection([
+        exports.i({
+            type: exports.l('Feature'),
+            geometry: exports.u([exports.GeometryObjectIO, io.null]),
+            properties: propType,
+        }),
+        exports.p({
+            id: exports.u([io.string, io.number]),
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], name);
+    exports.FeatureCollectionIO = io.intersection([
+        exports.i({
+            type: exports.l('FeatureCollection'),
+            features: exports.a(exports.FeatureIO),
+        }),
+        exports.p({
+            bbox: exports.BoundingBoxIO,
+        }),
+    ], 'FeatureCollectionIO');
+});
 //# sourceMappingURL=index.js.map
--- out/test/index.js
+++ out/test/index.js
@@ -1,158 +1,169 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const io = require("io-ts");
-const src_1 = require("../src");
-exports.geojsonLine = {
-    'type': 'FeatureCollection',
-    'features': [
-        {
-            'type': 'Feature',
-            'properties': {},
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [
-                    [
-                        -54.4921875,
-                        39.36827914916014,
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "io-ts", "../src"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.myFeatureGood = exports.myFeatureBad = exports.MyFeatureType = exports.MyPropType = exports.featureBad = exports.featureGood = exports.geojsonMissingProperties = exports.geojsonLine = void 0;
+    const io = require("io-ts");
+    const src_1 = require("../src");
+    exports.geojsonLine = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [
+                        [
+                            -54.4921875,
+                            39.36827914916014,
+                        ],
+                        [
+                            18.984375,
+                            59.712097173322924,
+                        ],
+                        [
+                            33.3984375,
+                            1.4061088354351594,
+                        ],
+                        [
+                            -36.9140625,
+                            12.211180191503997,
+                        ],
+                        [
+                            -54.140625,
+                            37.71859032558816,
+                        ],
+                        [
+                            82.265625,
+                            -10.833305983642491,
+                        ],
                     ],
-                    [
-                        18.984375,
-                        59.712097173322924,
+                },
+            },
+        ],
+    };
+    exports.geojsonMissingProperties = {
+        'type': 'FeatureCollection',
+        'features': [{
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [0, 0],
+                },
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [[4e6, -2e6], [8e6, 2e6]],
+                },
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [[4e6, 2e6], [8e6, -2e6]],
+                },
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]],
+                },
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'MultiLineString',
+                    'coordinates': [
+                        [[-1e6, -7.5e5], [-1e6, 7.5e5]],
+                        [[1e6, -7.5e5], [1e6, 7.5e5]],
+                        [[-7.5e5, -1e6], [7.5e5, -1e6]],
+                        [[-7.5e5, 1e6], [7.5e5, 1e6]],
                     ],
-                    [
-                        33.3984375,
-                        1.4061088354351594,
+                },
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'MultiPolygon',
+                    'coordinates': [
+                        [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6], [-3e6, 6e6]]],
+                        [[[-2e6, 6e6], [-2e6, 8e6], [0, 8e6], [0, 6e6]]],
+                        [[[1e6, 6e6], [1e6, 8e6], [3e6, 8e6], [3e6, 6e6]]],
                     ],
-                    [
-                        -36.9140625,
-                        12.211180191503997,
-                    ],
-                    [
-                        -54.140625,
-                        37.71859032558816,
-                    ],
-                    [
-                        82.265625,
-                        -10.833305983642491,
-                    ],
-                ],
-            },
-        },
-    ],
-};
-exports.geojsonMissingProperties = {
-    'type': 'FeatureCollection',
-    'features': [{
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [0, 0],
-            },
-        }, {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [[4e6, -2e6], [8e6, 2e6]],
-            },
-        }, {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [[4e6, 2e6], [8e6, -2e6]],
-            },
-        }, {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Polygon',
-                'coordinates': [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]],
-            },
-        }, {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'MultiLineString',
-                'coordinates': [
-                    [[-1e6, -7.5e5], [-1e6, 7.5e5]],
-                    [[1e6, -7.5e5], [1e6, 7.5e5]],
-                    [[-7.5e5, -1e6], [7.5e5, -1e6]],
-                    [[-7.5e5, 1e6], [7.5e5, 1e6]],
-                ],
-            },
-        }, {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'MultiPolygon',
-                'coordinates': [
-                    [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6], [-3e6, 6e6]]],
-                    [[[-2e6, 6e6], [-2e6, 8e6], [0, 8e6], [0, 6e6]]],
-                    [[[1e6, 6e6], [1e6, 8e6], [3e6, 8e6], [3e6, 6e6]]],
-                ],
-            },
-        }, {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'GeometryCollection',
-                'geometries': [{
-                        'type': 'LineString',
-                        'coordinates': [[-5e6, -5e6], [0, -5e6]],
-                    }, {
-                        'type': 'Point',
-                        'coordinates': [4e6, -5e6],
-                    }, {
-                        'type': 'Polygon',
-                        'coordinates': [[[1e6, -6e6], [2e6, -4e6], [3e6, -6e6]]],
-                    }],
-            },
-        }],
-};
-exports.featureGood = {
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        coordinates: [0, 0],
-    },
-    properties: null,
-};
-exports.featureBad = {
-    type: 'Feature',
-    geometry: {
+                },
+            }, {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'GeometryCollection',
+                    'geometries': [{
+                            'type': 'LineString',
+                            'coordinates': [[-5e6, -5e6], [0, -5e6]],
+                        }, {
+                            'type': 'Point',
+                            'coordinates': [4e6, -5e6],
+                        }, {
+                            'type': 'Polygon',
+                            'coordinates': [[[1e6, -6e6], [2e6, -4e6], [3e6, -6e6]]],
+                        }],
+                },
+            }],
+    };
+    exports.featureGood = {
         type: 'Feature',
-        coordinates: [0, 0],
-    },
-    properties: null,
-};
-exports.MyPropType = io.interface({
-    a: io.string,
-    b: io.number,
+        geometry: {
+            type: 'Point',
+            coordinates: [0, 0],
+        },
+        properties: null,
+    };
+    exports.featureBad = {
+        type: 'Feature',
+        geometry: {
+            type: 'Feature',
+            coordinates: [0, 0],
+        },
+        properties: null,
+    };
+    exports.MyPropType = io.interface({
+        a: io.string,
+        b: io.number,
+    });
+    exports.MyFeatureType = src_1.PartialFeatureIO(exports.MyPropType, 'MyFeatureType');
+    exports.myFeatureBad = {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [0, 0],
+        },
+        properties: {
+            a: 1,
+            b: 'a',
+        },
+    };
+    exports.myFeatureGood = {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [0, 0],
+        },
+        properties: {
+            a: 'a',
+            b: 1,
+        },
+    };
+    const good = (msg) => () => console.log(`GOOD ${msg}`);
+    const bad = (msg) => () => { throw (new Error(`BAD ${msg}`)); };
+    src_1.FeatureCollectionIO.decode(exports.geojsonLine).fold(bad('Line did not validate but it should'), good('geojsonLine validates as it should'));
+    src_1.FeatureCollectionIO.decode(exports.geojsonMissingProperties).fold(good('geojsonMissingProperties fails to validate as it should'), bad('missingProperties should not validate but it did'));
+    src_1.FeatureIO.decode(exports.featureGood).fold(bad('featureGood should validate'), good('featureGood validates as it should'));
+    src_1.FeatureIO.decode(exports.featureBad).fold(good('featureBad fails to validate as it should'), bad('featureBad should not validate'));
+    exports.MyFeatureType.decode(exports.myFeatureGood).fold(bad('myFeatureGood should validate'), good('myFeatureGood validates as it should'));
+    exports.MyFeatureType.decode(exports.myFeatureBad).fold(good('myFeatureBad fails to validate as it should'), bad('myFeatureBad should not validate'));
 });
-exports.MyFeatureType = src_1.PartialFeatureIO(exports.MyPropType, 'MyFeatureType');
-exports.myFeatureBad = {
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        coordinates: [0, 0],
-    },
-    properties: {
-        a: 1,
-        b: 'a',
-    },
-};
-exports.myFeatureGood = {
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        coordinates: [0, 0],
-    },
-    properties: {
-        a: 'a',
-        b: 1,
-    },
-};
-const good = (msg) => () => console.log(`GOOD ${msg}`);
-const bad = (msg) => () => { throw (new Error(`BAD ${msg}`)); };
-src_1.FeatureCollectionIO.decode(exports.geojsonLine).fold(bad('Line did not validate but it should'), good('geojsonLine validates as it should'));
-src_1.FeatureCollectionIO.decode(exports.geojsonMissingProperties).fold(good('geojsonMissingProperties fails to validate as it should'), bad('missingProperties should not validate but it did'));
-src_1.FeatureIO.decode(exports.featureGood).fold(bad('featureGood should validate'), good('featureGood validates as it should'));
-src_1.FeatureIO.decode(exports.featureBad).fold(good('featureBad fails to validate as it should'), bad('featureBad should not validate'));
-exports.MyFeatureType.decode(exports.myFeatureGood).fold(bad('myFeatureGood should validate'), good('myFeatureGood validates as it should'));
-exports.MyFeatureType.decode(exports.myFeatureBad).fold(good('myFeatureBad fails to validate as it should'), bad('myFeatureBad should not validate'));
 //# sourceMappingURL=index.js.map
